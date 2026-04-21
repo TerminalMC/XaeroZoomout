9ba8d853ba705b44a321b5cb7271438e902a8ed9
NotRyken <127091011+NotRyken@users.noreply.github.com>
2026-04-21 17:25:20 +0800

    Update to mc26.1.2 and template v16

--- fabric/src/main/java/dev/terminalmc/xaerozoomout/XaeroZoomoutFabric.java
+++ fabric/src/main/java/dev/terminalmc/xaerozoomout/XaeroZoomoutFabric.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 TerminalMC
+ * Copyright 2026 TerminalMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package dev.terminalmc.xaerozoomout;
 
 import net.fabricmc.api.ClientModInitializer;
 
+@SuppressWarnings("unused")
 public class XaeroZoomoutFabric implements ClientModInitializer {
 
     @Override
